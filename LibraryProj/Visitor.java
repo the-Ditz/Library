b672fcf3fff5cb0efff04dc03a27e590dc43cd01
John Stark <johnstar@amazon.com>
2017-10-04 14:03:34 -0700

    Visitor.java -- Updated checkOutMedia and checkInMedia to handle Media objects. Added baseline documenation.

--- LibraryProj/Visitor.java
+++ LibraryProj/Visitor.java
@@ -2,10 +2,6 @@ import java.math.BigDecimal;
 
 public class Visitor {
     
-    //name: String
-    //ownedBooks: Inventory
-    //overdueBalance: BigDecimal
-    
     final private String name;
     final private Inventory checkedOutMedia;
     final private BigDecimal overdueBalance;
@@ -16,26 +12,32 @@ public class Visitor {
         overdueBalance = new BigDecimal(0.00);
     }
     
+    // returns the visitor's name
     public String getName() {
         return name;
     }
     
+    // returns an Inventory of what the visitor has checked out
     public Inventory getCheckedOutMedia(String name) {
         return checkedOutMedia;
     }
     
+    // returns a visitor's overdue balance
     public BigDecimal getOverdueBalance(String name) {
         return overdueBalance;
     }
     
-    public void checkOutMedia(String mediaTitle) {
-        checkedOutMedia.add(mediaTitle);
+    // appends the visitor's checkedOutMedia Inventory with a passed media object
+    public void checkOutMedia(Media mediaTitle) {
+        checkedOutMedia.addItem(mediaTitle);
     }
     
-    public void checkInMedia(String mediaTitle) {
-        checkedOutMedia.remove(mediaTitle);
+    // removes the passed media object from the visitor's checkedOutMedia Inventory. 
+    public void checkInMedia(Media mediaTitle) {
+        checkedOutMedia.removeItem(mediaTitle);
     }
     
+    // updates the visitor's overdueBalance 
     public void setOverdueBalance(BigDecimal amount) {
         overdueBalance.add(amount);
     }
