f24a2df19f544009ee2a7c537317ba88e390ced9
Josh Wittlieff <wittlief@amazon.com>
2017-10-04 13:34:29 -0700

    Add Inventory class to store list of Media

---
+++ LibraryProj/Inventory.java
@@ -0,0 +1,47 @@
+package GroupProj.LibrarySystem;
+
+import java.util.ArrayList;
+
+public class Inventory {
+    private ArrayList<Media> contents = new ArrayList<>();
+
+    /*
+     * Add a media item if not a duplicate.
+     */
+    public void addItem(Media media) {
+        if (checkIfInInventory(media.getTitle()) != -1) {
+            contents.add(media);
+        }
+    }
+
+    /*
+     * Remove an item if in inventory.
+     */
+    public void removeItem(Media media) {
+        int indexOfItem = checkIfInInventory(media.getTitle());
+        
+        if (indexOfItem != -1) {
+            contents.remove(indexOfItem);
+        }
+    }
+
+    /*
+     * Return number of items in inventory.
+     */
+    public int getInventorySize() {
+        return contents.size();
+    }
+
+    /*
+     * Return index of item if found in inventory; searches by title.
+     */
+    public int checkIfInInventory(String name) {
+        for (int i = 0; i < contents.size(); i++) {
+            if (contents.get(i).getTitle().equals(name)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
