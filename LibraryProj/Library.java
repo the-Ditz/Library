4bfd03a77b50c748720af9d05cb9d782dfd4ee3b
Justin Hill <hiljusti@amazon.com>
2017-10-04 13:35:54 -0700

    Add a non-functioning idea of what a Library should have/do

---
+++ LibraryProj/Library.java
@@ -0,0 +1,33 @@
+package LibraryProj;
+
+import java.util.ArrayList;
+
+public class Library {
+    Inventory inventory = new Inventory();
+    ArrayList<Visitor> visitors = new ArrayList<>();
+
+    public ArrayList<Media> searchMedia(String term) {
+        ArrayList<Media> matches = new ArrayList<>();
+        for (Media media: inventory.getMedia()) {
+            if (media.match(term)) {
+                matches.add(media.clone());
+            }
+        }
+        return matches;
+    }
+
+    public void checkOut(Media media) {
+        if (!media instanceof CheckOutable) {
+            throw new IllegalArgumentException("Can't check out Media that is not also CheckOutable.")
+        }
+        return;
+    }
+
+    public void addMedia(Media media) {
+        inventory.addItem(media);
+    }
+
+    public void addVisitor(Visitor visitor) {
+        visitors.add(visitor);
+    }
+}
