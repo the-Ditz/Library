3fb21d965a57bac43f47194315ea9e4914ae8675
Justin Hill <hiljusti@amazon.com>
2017-10-04 14:48:55 -0700

    rename checkIfInSomethingSomething to indexOf

--- LibraryProj/Inventory.java
+++ LibraryProj/Inventory.java
@@ -7,7 +7,7 @@ public class Inventory {
      * Add a media item if not a duplicate.
      */
     public void addItem(Media media) {
-        if (checkIfInInventory(media.getTitle()) != -1) {
+        if (indexOf(media.getTitle()) != -1) {
             contents.add(media);
         }
     }
@@ -16,7 +16,7 @@ public class Inventory {
      * Remove an item if in inventory.
      */
     public void removeItem(Media media) {
-        int indexOfItem = checkIfInInventory(media.getTitle());
+        int indexOfItem = indexOf(media.getTitle());
         
         if (indexOfItem != -1) {
             contents.remove(indexOfItem);
@@ -47,7 +47,7 @@ public class Inventory {
     /*
      * Return index of item if found in inventory; searches by title.
      */
-    public int checkIfInInventory(String name) {
+    public int indexOf(String name) {
         for (int i = 0; i < contents.size(); i++) {
             if (contents.get(i).getTitle().equals(name)) {
                 return i;
